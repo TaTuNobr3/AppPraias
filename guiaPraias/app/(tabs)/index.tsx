cdf4e4a6b60c60bf806fc9b9943064d6358b608b
Pedro <pedrocruzmoretto@gmail.com>
2025-10-23 17:54:57 +0000

    Versão 0.2

--- guiaPraias/app/(tabs)/index.tsx
+++ guiaPraias/app/(tabs)/index.tsx
@@ -1,98 +1,73 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React from
+import { StyleSheet, View, Text, Imagem, StatusBar, TouchableOpaciyt, SafeAreaView} from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+const CategoryCard = ({ backgroundcolor, title, titleColor = '#fff'})  =>
+(
+    <TouchableOpaciyt styles={[styles.card, {backgroundcolor}]}
+        <Text styles={[styles.cardText, {color: titlecolor}]}></Text>
+    </TouchableOpaciyt>
+);
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+export default function Home() {
+    return(
+        <SafeAreaView style={styles.container}>
+            <StatusBar barStyle="dark-content" />
+            <Imagem
+                souce={require('../assents/header-image.jpg')}
+                style={styles.headerimagem} />
+                </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+    container: {
+        flex: 1,
+        backgroundcolor: '#fff',
+        alignItems: 'center',
+    },
+    headerimagem: {
+        width: '100%',
+        height:'250px',
+        resizeMode: 'cover'
+    },
+    titleContainer: {
+        marginVertical: 24,
+        alignItems: 'center',
+    },
+    MainTitle: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#1e63ff',
+    },
+    subTitle: 16,
+    color: '#48d6d2',
+    marginTop: 4,
+},
+grid: {
+    width: '90%',
+},
+row:{
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+},
+card:{
+    width: '48%',
+    height: 120,
+    borderRadius: 20,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    shadowcolor: '#000',
+    shadowoffset: {
+        width: 0,
+        height: 2,
+    },
+    shadowopacity: 0.25,
+    shadowradius: 3.84,
+    elevation: 5,
+},
+cardText: {
+    fontSize: 18,
+    fontweight: 'bold',
+},   
 });
